2e0130ee0f47e8811d07138f3329c74d906cc27e
James Houghton <jamesthoughton@gmail.com>
2018-09-30 20:56:46 -0400

    Make docker compose work

--- start.sh
+++ start.sh
@@ -6,14 +6,13 @@ APP=partex
 
 case $1 in
     "django")
-        docker run -it --name web -p 8000:8000 --link mysql:db -v $PWD/app:/app tp33/django \
-            # mod_wsgi-express start-server --reload-on-changes --working-directory /app/$APP /app/$APP/$APP/wsgi.py
+        docker-compose up
         ;;
     "django-cli")
         docker exec -it web /bin/bash
         ;;
     "mysql")
-        docker run --name mysql -d -e MYSQL\_ROOT\_PASSWORD='$3cureUS' -v db:/var/lib/mysql mysql:5.7.23
+        docker run --name mysql -d --env-file db/vars.env -v db:/var/lib/mysql mysql:5.7.23
         ;;
     "mysql-cli")
         docker run -it --name mysql-cmdline --link mysql:db mysql:5.7.23 bash
