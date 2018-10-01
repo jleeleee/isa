5a545a9168b7559037a246ef9150e672817013f9
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-09-30 23:14:19 -0400

    listings model

--- app/partex/partex/apps/listings/models.py
+++ app/partex/partex/apps/listings/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from ..apps.users import User
+from ..apps.reviews import review
 
 # Create your models here.
+
+class Listing(models.Model)
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.BooleanField(default=True)
+    description = models.TextField(blank=True)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
