d1acad1232c8ba3aa8f438f5072be79b77c31865
Jihyeong Lee <leejh1998@hotmail.com>
2018-09-30 23:16:10 -0400

    Added model for reviews

--- app/partex/partex/apps/reviews/models.py
+++ app/partex/partex/apps/reviews/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from ..apps.users.models import User
+from ..apps.listings.models import Listing
+
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=300)
+    rating = models.IntegerField()
+    body = models.TextField()
+
+class UserReview(Review):
+    author = models.ForeignKey(User)
+    subject = models.ForeignKey(User)
+
+class ListingReview(Review):
+    author = models.ForeignKey(User)
+    subject = models.ForeignKey(Listing)
