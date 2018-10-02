d3556bdc8dacaf11b52059c945e86c24d48d3b74
Jihyeong Lee <leejh1998@hotmail.com>
2018-10-02 19:17:55 -0400

    added get_dict to listings and reviews

--- app/partex/apps/listings/models.py
+++ app/partex/apps/listings/models.py
@@ -7,6 +7,7 @@ class AbstractItem(models.Model):
     # Back Accessors:
     #   reviews
     #   listings
+    EXPOSED_FIELDS = ["name", "generic_description"]
     name = models.CharField(max_length=200, unique=True)
     generic_description = models.TextField(blank=True)
     #reference_image = models.ImageField(upload_to='ref_images/{}/'.format(name), blank=True)
@@ -14,7 +15,11 @@ class AbstractItem(models.Model):
     def __str__(self):
         return self.name
 
+    def get_dict(self):
+        return { d: getattr(self, d) for d in AbstractItem.EXPOSED_FIELDS }
+
 class Listing(models.Model):
+    EXPOSED_FIELDS = ["name", "price", "status", "description", "seller", "base_item"]
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.BooleanField(default=True)
@@ -27,3 +32,6 @@ class Listing(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_dict(self):
+        return { d: getattr(self, d) for d in Listing.EXPOSED_FIELDS }
--- app/partex/apps/reviews/models.py
+++ app/partex/apps/reviews/models.py
@@ -5,6 +5,7 @@ from ..listings.models import Listing, AbstractItem
 
 # Create your models here.
 class Review(models.Model):
+    EXPOSED_FIELDS = ["title", "rating", "body", "author"]
     title = models.CharField(max_length=300)
     rating = models.IntegerField()
     body = models.TextField()
@@ -12,15 +13,23 @@ class Review(models.Model):
 
     def __str__(self):
         return self.title
+    def get_dict(self):
+        return { d: getattr(self, d) for d in Review.EXPOSED_FIELDS }
 
 class UserReview(Review):
+    EXPOSED_FIELDS = Review.EXPOSED_FIELDS.extend(["subject"])
     subject = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
 
     def __str__(self):
         return self.title
+    def get_dict(self):
+        return { d: getattr(self, d) for d in UserReview.EXPOSED_FIELDS }
 
 class ItemReview(Review):
+    EXPOSED_FIELDS = Review.EXPOSED_FIELDS.extend(["subject"])
     subject = models.ForeignKey(AbstractItem, on_delete=models.CASCADE, related_name="reviews")
 
     def __str__(self):
         return self.title
+    def get_dict(self):
+        return { d: getattr(self, d) for d in ItemReview.EXPOSED_FIELDS }
