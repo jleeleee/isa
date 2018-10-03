0e628d87bb2b1cb48b38d31b8ad5cb66cda23017
Jihyeong Lee <leejh1998@hotmail.com>
2018-10-02 22:40:12 -0400

    fixed something

--- app/partex/apps/reviews/models.py
+++ app/partex/apps/reviews/models.py
@@ -5,7 +5,7 @@ from ..listings.models import Listing, AbstractItem
 
 # Create your models here.
 class Review(models.Model):
-    EXPOSED_FIELDS = ["id", "title", "rating", "body", "author", "date_created"]
+    EXPOSED_FIELDS = ["id", "title", "rating", "body", "date_created"]
     title = models.CharField(max_length=300)
     rating = models.IntegerField()
     body = models.TextField()
@@ -16,22 +16,26 @@ class Review(models.Model):
     def __str__(self):
         return self.title
     def get_dict(self):
-        return { d: getattr(self, d) for d in Review.EXPOSED_FIELDS }
+        ret_dict = { d: getattr(self, d) for d in Review.EXPOSED_FIELDS }
 
 class UserReview(Review):
-    EXPOSED_FIELDS = Review.EXPOSED_FIELDS.extend(["subject"])
     subject = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviews")
 
     def __str__(self):
         return self.title
     def get_dict(self):
-        return { d: getattr(self, d) for d in UserReview.EXPOSED_FIELDS }
+        ret_dict = { d: getattr(self, d) for d in UserReview.EXPOSED_FIELDS }
+        ret_dict["author"] = self.author.id
+        ret_dict["subject"] = self.subject.id
+        return ret_dict
 
 class ItemReview(Review):
-    EXPOSED_FIELDS = Review.EXPOSED_FIELDS.extend(["subject"])
     subject = models.ForeignKey(AbstractItem, on_delete=models.CASCADE, related_name="reviews")
 
     def __str__(self):
         return self.title
     def get_dict(self):
-        return { d: getattr(self, d) for d in ItemReview.EXPOSED_FIELDS }
+        ret_dict = { d: getattr(self, d) for d in ItemReview.EXPOSED_FIELDS }
+        ret_dict["author"] = self.author.id
+        ret_dict["subject"] = self.subject.id
+        return ret_dict
--- app/partex/apps/reviews/views.py
+++ app/partex/apps/reviews/views.py
@@ -25,10 +25,11 @@ def user_info(request, id_):
 @csrf_exempt
 @require_http_methods(["POST"])
 def user_create(request):
-    required_fields = ["title", "rating", "body","date_created", "author", "subject"]
+    required_fields = ["title", "rating", "body","author", "subject"]
     if any(map(lambda k: k not in request.POST, required_fields)):
         return JsonResponse({
             "ok": False,
+            "fuck": str(request.POST),
             "message": "Missing a required field: (one of {})".format(required_fields)
         })
 
@@ -36,7 +37,6 @@ def user_create(request):
             title = request.POST["title"],
             rating = request.POST["rating"],
             body = request.POST["body"],
-            date_created = request.POST["date_created"],
             author = request.POST["author"],
             subject = request.POST["subject"]
         )
@@ -69,7 +69,6 @@ def user_update(request, id_):
     r.title = request.POST["title"],
     r.rating = request.POST["rating"],
     r.body = request.POST["body"],
-    r.date_created = request.POST["date_created"],
     r.author = request.POST["author"],
     r.subject = request.POST["subject"]
 
@@ -79,6 +78,8 @@ def user_update(request, id_):
         "ok": True,
         "result": u.get_dict()
     })
+
+
 
 # ItemReview Views
 def item_index(request):
@@ -99,7 +100,7 @@ def item_info(request, id_):
 @csrf_exempt
 @require_http_methods(["POST"])
 def item_create(request):
-    required_fields = ["title", "rating", "body", "author", "date_created", "subject"]
+    required_fields = ["title", "rating", "body", "author", "subject"]
     if any(map(lambda k: k not in request.POST, required_fields)):
         return JsonResponse({
             "ok": False,
@@ -110,7 +111,6 @@ def item_create(request):
             title = request.POST["title"],
             rating = request.POST["rating"],
             body = request.POST["body"],
-            date_created = request.POST["date_created"],
             author = request.POST["author"],
             subject = request.POST["subject"]
         )
@@ -143,7 +143,6 @@ def item_update(request, id_):
     r.title = request.POST["title"]
     r.rating = request.POST["rating"]
     r.body = request.POST["body"]
-    r.date_created = request.POST["date_created"]
     r.author = request.POST["author"]
     r.subject = request.POST["subject"]
 
