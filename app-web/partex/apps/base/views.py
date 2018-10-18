47aa9c605f52bea2d63b42acc78c423a3c541a82
James Houghton <jhoughton@virginia.edu>
2018-10-18 19:19:21 -0400

    Fixup some UI and ALLOWED_HOSTS

--- app-web/partex/apps/base/views.py
+++ app-web/partex/apps/base/views.py
@@ -7,20 +7,23 @@ import json
 # Create your views here.
 
 def index(request):
+    context = {}
+
     try:
         req = urllib.request.Request("http://exp:8000/v1/api/listings")
         resp_json = urllib.request.urlopen(req).read().decode('utf-8')
         resp = json.loads(resp_json)
     except urllib.error.HTTPError as e:
+        """
         return JsonResponse({
             "ok": "False",
             "error": str(e.reason),
             "str": str(e)
         })
+        context = {
+            "ok": resp["ok"],
+            "response": resp_json
+        }
+        """
 
-
-    context = {
-        "ok": resp["ok"],
-        "response": resp_json
-    }
     return render(request, "index.html", context)
