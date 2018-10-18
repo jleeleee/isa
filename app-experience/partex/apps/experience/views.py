723dad7cdf4ebbd0d3147bfac6f5dca2fde9f1b5
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-10-18 19:34:46 -0400

    views headers

--- app-experience/partex/apps/experience/views.py
+++ app-experience/partex/apps/experience/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_http_methods
 
+import urllib.request
+import urllib.parse
+import json
+
 # Create your views here.
 
 def index(request):
@@ -10,3 +14,12 @@ def index(request):
         "ok":True,
         "result":"experience"
     })
+
+def home(request):
+    pass
+
+def listing(request):
+    pass
+
+def abstractItem(request):
+    pass
