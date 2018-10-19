73454cf6c7efcd6c8e4823ac3d9d8642c5144be8
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-10-18 22:04:40 -0400

    add listing test

--- app-models/partex/apps/listings/tests.py
+++ app-models/partex/apps/listings/tests.py
@@ -14,12 +14,14 @@ class GetListingsFromAbstract(TestCase):
                 )
         self.AI = AbstractItem.objects.create(
                 name="AI_name",
-                generic_description="AI_descr",
+                generic_description="AI_descr"
                 )
+        self.AI.save()
         self.BI = AbstractItem.objects.create(
                 name="BI_name",
-                generic_description="BI_descr",
+                generic_description="BI_descr"
                 )
+        self.BI.save()
         self.L1 = Listing.objects.create(
                 name="L1_name",
                 price=1.00,
@@ -28,6 +30,7 @@ class GetListingsFromAbstract(TestCase):
                 base_item=self.AI,
                 seller=self.Seller
             )
+        self.L1.save()
         self.L2 = Listing.objects.create(
                 name="L2_name",
                 price=2.00,
@@ -36,6 +39,7 @@ class GetListingsFromAbstract(TestCase):
                 base_item=self.AI,
                 seller=self.Seller
             )
+        self.L2.save()
         self.L3 = Listing.objects.create(
                 name="L3_name",
                 price=3.00,
@@ -44,6 +48,7 @@ class GetListingsFromAbstract(TestCase):
                 base_item=self.AI,
                 seller=self.Seller
             )
+        self.L3.save()
         self.L4 = Listing.objects.create(
                 name="L4_name",
                 price=4.00,
@@ -52,10 +57,13 @@ class GetListingsFromAbstract(TestCase):
                 base_item=self.BI,
                 seller=self.Seller
             )
-    def test_get_listing_from_abstract(self):
-        print("testing test")
-        self.assertEqual(True, True)
+        self.L4.save()
 
+    def test_get_listing_from_abstract(self):
+        ai = AbstractItem.objects.get(name="AI_name")
+        i_listings = ai.listings.all().values_list('id', flat=True).order_by('id')
+        listing_ids = [self.L1.id, self.L2.id, self.L3.id]
+        self.assertEqual(list(i_listings), listing_ids)
     def tearDown(self):
         self.AI.delete()
         self.BI.delete()
