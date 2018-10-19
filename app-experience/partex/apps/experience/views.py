7ea315c708dcaaa795478c8219be501c874ed5c0
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-10-18 20:58:07 -0400

    add experience layer for listings

--- app-experience/partex/apps/experience/views.py
+++ app-experience/partex/apps/experience/views.py
@@ -10,10 +10,8 @@ import json
 # Create your views here.
 
 def homepage(request):
-    context = {}
-
     try:
-        req = urllib.request.Request("http://models:8000/api/v1/listings")
+        req = urllib.request.Request("http://models:8000/api/v1/listings/recent")
         resp_json = urllib.request.urlopen(req).read().decode('utf-8')
         resp = json.loads(resp_json)
     except urllib.error.HTTPError as e:
@@ -28,15 +26,38 @@ def homepage(request):
         "listings": resp["result"]
     })
 
-
-def listing(request):
+def listing(request, _id):
     # Returns the page containing info about the listing
-    pass
+    try:
+        req = urllib.request.Request("http://models:8000/api/v1/listings/" + _id)
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        resp = json.loads(resp_json)
+    except urllib.error.HTTPError as e:
+        return JsonResponse({
+            "ok": "False",
+            "error": str(e.reason),
+            "str": str(e)
+        })
+
+    return JsonResponse({
+        "ok": True,
+        "listing": resp["result"]
+    })
 
 def all_listings(request):
     # Returns all active listings
-    pass
+    try:
+        req = urllib.request.Request("http://models:8000/api/v1/listings/")
+        resp_json = urllib.request.urlopen(req).read().decode('utf-8')
+        resp = json.loads(resp_json)
+    except urllib.error.HTTPError as e:
+        return JsonResponse({
+            "ok": "False",
+            "error": str(e.reason),
+            "str": str(e)
+        })
 
-def all_reviews(request):
-    # Returns all reviews about a given user or item
-    pass
+    return JsonResponse({
+        "ok": True,
+        "listings": resp["result"]
+    })
