73502132ddba0e17874878fa43319ec447b2b50c
Jihyeong Lee <leejh1998@hotmail.com>
2018-10-18 21:00:39 -0400

    testing shell

--- app-models/partex/apps/listings/tests.py
+++ app-models/partex/apps/listings/tests.py
@@ -1,17 +1,58 @@
 from django.test import TestCase
-
+from .models import AbstractItem, Listing
 # Create your tests here.
 
-class getListingsFromAbstract(TestCase):
+class GetListingsFromAbstract(TestCase):
     # Getting a set of listings by looking at an abstract item
     def setUp(self):
+        self.AI = AbstractItem.objects.create(
+                name="AI_name", 
+                generic_description="AI_descr",
+                )
+        self.BI = AbstractItem.objects.create(
+                name="BI_name", 
+                generic_description="BI_descr",
+                )
+        self.L1 = Listing.objects.create(
+                name="L1_name",
+                price=1.00,
+                status=True,
+                description="L1_descr",
+                base_item=AI
+            )
+        self.L2 = Listing.objects.create(
+                name="L2_name",
+                price=1.00,
+                status=False,
+                description="L2_descr",
+                base_item=AI
+            )
+        self.L3 = Listing.objects.create(
+                name="L3_name",
+                price=1.00,
+                status=True,
+                description="L3_descr",
+                base_item=AI
+            )
+        self.L4 = Listing.objects.create(
+                name="L4_name",
+                price=1.00,
+                status=True,
+                description="L4_descr",
+                base_item=BI
+            )
+    def test_get_listing_from_abstract(self):
         pass
-    def success_response(self):
-        pass
-    def fails_invalid(self):
-        pass
+        
+        
+
     def tearDown(self):
-        pass
+        self.AI.delete()
+        self.BI.delete()
+        self.LI.delete()
+        self.L2.delete()
+        self.L3.delete()
+        self.L4.delete()
 
 class getReviewsOfSeller(TestCase):
     # Getting the reviews of the seller of a listing
