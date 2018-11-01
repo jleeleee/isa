ca125ba522696e64c508fb09c0f29725c4b65d37
James Houghton <jhoughton@virginia.edu>
2018-11-01 18:31:46 -0400

    experience -> base

--- app-experience/partex/settings.py
+++ app-experience/partex/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'partex.apps.experience'
+    'partex.apps.base'
 ]
 
 MIDDLEWARE = [
--- app-experience/partex/urls.py
+++ app-experience/partex/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from .apps.experience import views
+from .apps.base import views
 
 apipatterns = [
     path('home', views.homepage),
