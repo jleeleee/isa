e6193f5d175575f4841360752ef7e834e43e8779
James Houghton <jhoughton@virginia.edu>
2018-11-01 17:15:51 -0400

    Fixes

--- app-web/partex/apps/base/views.py
+++ app-web/partex/apps/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
+from django.urls import reverse
 from . import forms
 import urllib.request
 import urllib.parse
