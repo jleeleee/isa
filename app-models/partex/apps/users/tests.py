e912feb9dc80ec077401053123cb2336ea57cc4d
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-11-01 21:52:27 -0400

    authenticator test

--- app-models/partex/apps/users/tests.py
+++ app-models/partex/apps/users/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from .models import User
+from .models import Authenticator
 from ..listings.models import Listing
+from django.utils import timezone
 
 # Create your tests here.
 
@@ -51,3 +53,58 @@ class GetUsersListings(TestCase):
         self.l1.delete()
         self.l2.delete()
         self.l3.delete()
+
+class AuthenticatorTests(TestCase):
+    # Testing authenticator
+    def setUp(self):
+        self.user1 = User.objects.create(
+            username="user1",
+            first_name="fn",
+            last_name="ln",
+            email="a@a.a"
+        )
+        self.user2 = User.objects.create(
+            username="user2",
+            first_name="fn",
+            last_name="ln",
+            email="b@b.b"
+        )
+        self.user3 = User.objects.create(
+            username="user3",
+            first_name="fn",
+            last_name="ln",
+            email="c@c.c"
+        )
+        self.auth1 = Authenticator.objects.create(
+            user = self.user1
+        )
+        self.auth2 = Authenticator.objects.create(
+            user = self.user2
+        )
+    def testAuthenticators(self):
+        auth1 = Authenticator.objects.filter(
+            user__id = self.user1.id,
+            authenticator = self.auth1.authenticator
+        )
+        self.assertEquals(auth1.exists(), True)
+        self.assertEquals(auth1.get().is_expired(), False)
+        auth2 = Authenticator.objects.filter(
+            user__id = self.user2.id,
+            authenticator = self.auth2.authenticator
+        )
+        self.assertEquals(auth2.exists(), True)
+        _auth2 = auth2.get()
+        _auth2.datetime_created += timezone.timedelta(days=-7)
+        _auth2.save()
+        self.assertEquals(auth2.get().is_expired(), True)
+        auth3 = Authenticator.objects.filter(
+            user__id = self.user3.id,
+            authenticator = 2
+        )
+        self.assertEquals(auth3.exists(), False)
+    def tearDown(self):
+        self.user1.delete()
+        self.user2.delete()
+        self.user3.delete()
+        self.auth1.delete()
+        self.auth2.delete()
