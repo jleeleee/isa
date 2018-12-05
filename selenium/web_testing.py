400800e2229d3fd30b2181945f64f7a639127e84
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-12-04 22:10:22 -0500

    add initial selenium testing

--- selenium/web_testing.py
+++ selenium/web_testing.py
@@ -2,21 +2,34 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+from time import sleep
+
 class SampleTest(unittest.TestCase):
 
     def setUp(self):
+        sleep(5)
         self.driver = webdriver.Remote(
             command_executor='http://selenium-chrome:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME)
 
-    # Add unit tests
-    def test_view_all_listings(self):
-        self.driver.get('http://web:8000/')
+    def test_connect(self):
+        driver = self.driver
+        driver.get('http://web:8000/')
         self.assertEqual(driver.title, "partex")
 
-        listings_button = self.driver.find_elements_by_xpath('//*[@id]')
-        for el in listings_button:
-            print(el.text)
+    # Add unit tests
+    def test_view_all_listings(self):
+        driver = self.driver
+        driver.get('http://web:8000/')
+        self.assertEqual(driver.title, "partex")
+
+        listings_button = self.driver.find_elements_by_xpath('//a[text()="View all listings"]')
+        print(listings_button)
+        listings_button[0].click()
+        listings_button = self.driver.find_elements_by_xpath('//a[text()="View all listings"]')
+        print(listings_button)
+
+        self.assertEqual(driver.title, "listings")
 
     def tearDown(self):
         self.driver.quit()
