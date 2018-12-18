6cd17bc0448a2d747245221950e94544c1e74fa5
Jihyeong Lee <leejh1998@hotmail.com>
2018-12-18 17:40:02 -0500

    recommendations without db

--- spark-data/spark.py
+++ spark-data/spark.py
@@ -1,5 +1,7 @@
 from pyspark import SparkContext
+import MySQLdb
 
+"""
 sc = SparkContext("spark://spark-master:7077", "PopularItems")
 
 data = sc.textFile("/tmp/data/access.log", 2)     # each worker loads a piece of the data file
@@ -14,4 +16,16 @@ for page_id, count in output:
     print ("page_id %s count %d" % (page_id, count))
 print ("Popular items done")
 
-sc.stop()
+"""
+def create_co_views(sc, data):
+    pairs = data.map(lambda line: line.split("\t"))
+    user_coviews = pairs.join(pairs).filter(lambda x: x[1][0] > x[1][1])
+    co_views = user_coviews.map(lambda x: ((int(x[1][0]), int(x[1][1])), int(x[0])))
+    co_views = co_views.groupByKey().map(lambda x: (x[0], len(x[1]))).filter(lambda x: x[1] >= 3)
+    co_views = co_views.collect()
+
+
+if __name__ == '__main__':
+    sc = SparkContext("spark://spark-master:7077", "PopularItems")
+    data = sc.textFile("/tmp/data/access.log")
+    create_co_views(sc, data)
