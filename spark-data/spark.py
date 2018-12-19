83e042e66333b86d55b2c8623aa620bc78b12267
Jihyeong Lee <leejh1998@hotmail.com>
2018-12-18 19:32:13 -0500

    thing

--- spark-data/spark.py
+++ spark-data/spark.py
@@ -1,7 +1,10 @@
 from pyspark import SparkContext
 import MySQLdb
-
+import sys
 def write_to_db(data):
+    for entry in data:
+        print("INSERT INTO recommendations(Page, Recos) VALUES ({}, \"{}\")".format(entry[0], entry[1]))
+
     db = MySQLdb.connect("db", "www", "$3cureUS", "cs4501")
     cursor = db.cursor()
     cursor.execute("USE cs4501")
@@ -11,16 +14,21 @@ def write_to_db(data):
             Recos varchar(255)
         )
     """)
-    cursor.execute("INSERT INTO recommendations(Page, Recos) VALUES ({}, {})".format(data))
+    for entry in data:
+        cursor.execute("INSERT INTO recommendations(Page, Recos) VALUES ({}, \"{}\")".format(entry[0], entry[1]))
     db.commit()
 
 
 def create_co_views(data):
-    pairs = data.map(lambda line: line.split("\t"))
-    user_coviews = pairs.join(pairs).filter(lambda x: x[1][0] > x[1][1])
-    co_views = user_coviews.map(lambda x: ((int(x[1][0]), int(x[1][1])), int(x[0])))
-    co_views = co_views.groupByKey().map(lambda x: (x[0], len(x[1]))).filter(lambda x: x[1] >= 3)
-    return co_views.collect()
+    pairs = data.map(lambda line: line.split(","))
+    pairs = pairs.map(lambda x: (x[0], ( x[1], x[2] )))
+    user_coviews = pairs.join(pairs).filter(lambda x: x[1][0][1] < x[1][1][1])
+    co_views = user_coviews.map(lambda x: ((x[1][0][0], x[1][1][0]), x[0]))
+    co_views = co_views.groupByKey().map(lambda x: (x[0], len(x[1])))#.filter(lambda x: x[1] >= 3)
+    views = co_views.map(lambda x: (x[0][0], x[0][1])).groupByKey()
+    views = views.map(lambda y: ( y[0], ", ".join(list(y[1])) )).collect()
+
+    return views
 
 
 
