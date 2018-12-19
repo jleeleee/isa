5f6c090b8b23ee00d4105cd4f6f57bd7c50084b2
Quinn Dawkins <quinn.dawkins@gmail.com>
2018-12-18 19:07:34 -0500

    switch from tabs to commans

--- spark-data/spark_kafka.py
+++ spark-data/spark_kafka.py
@@ -16,5 +16,5 @@ with open("/app/access.log", "a") as log:
 
     for message in consumer:
         pair = json.loads((message.value).decode('utf-8'))
-        log.write("\n{}\t{}\t{}".format(pair["user_id"], pair["item_id"], pair["time"]))
+        log.write("\n{},{},{}".format(pair["user_id"], pair["item_id"], pair["time"]))
         log.flush()
